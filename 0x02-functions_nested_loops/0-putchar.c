19a727e5d12de6192f35e15e99fe4703ec0d692a
celinehartze <celinehartze@gmail.com>
2022-06-07 12:18:30 -0700

    Task 0 commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
-
 /**
  * main - entry point
- *
  * Return: 0 
  */
 
@@ -11,9 +9,7 @@ int main(void)
 	char puts[7] = "putchar";
 	int i;
 	int v = '_';
-	
 	putchar(v);
-	
 	for (i = 0; i < 7; i++)
 	{
 		putchar(puts[i]);
