449b0c5e329a5406b2d1c75f8474ddee51b105ee
celinehartze <celinehartze@gmail.com>
2022-06-16 13:46:20 -0700

    1st commit

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,29 @@
+// Online C compiler to run C program online
+#include "main.h"
+
+/**
+* _strcat - concatenates two strings
+* @dest: string to be appended to
+* @src: string whose contents to append to src
+*
+* Definition:
+* This function appends the src string to the dest string,
+* overwriting the terminating null byte (\0) at the end of dest,
+* and then adds a terminating null byte
+*
+* Return: a pointer to the resulting string dest
+*/
+char *_strcat(char *dest, char *src)
+{
+	char *new_dest = dest;
+
+	while (*dest)
+		dest++;
+
+	while (*src)
+		*dest++ = *src++;
+
+	*dest = '\0';
+
+	return (new_dest);
+}
